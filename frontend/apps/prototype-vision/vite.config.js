750dc6bed633d57c0063c1bfd70f194490c3eb1b
DaSilvaAlves <183132027+DaSilvaAlves@users.noreply.github.com>
2026-01-11 14:32:38 -0500

    fix: force allow all hosts in vite config for both server and preview

--- frontend/apps/prototype-vision/vite.config.js
+++ frontend/apps/prototype-vision/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        allowedHosts: 'all'
+    },
+    preview: {
+        allowedHosts: 'all'
+    }
 
 
 })
