8d1c74a0ed8944bcc4652c484d29f9b60d685bad
Eurico Alves <euricojsalves@gmail.com>
2026-01-19 02:57:27 +0000

    chore: trigger Railway rebuild for marketplace

--- services/marketplace/src/index.ts
+++ services/marketplace/src/index.ts
@@ -73,3 +73,4 @@ process.on('SIGTERM', () => server.close(() => process.exit(0)));
 process.on('SIGINT', () => server.close(() => process.exit(0)));
 
 export default app;
+// Railway rebuild trigger
